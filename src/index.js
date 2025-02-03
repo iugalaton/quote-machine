54c1ff6c201399d59860e6cb177a29531de37f1b
juliagalaton <iulia.galaton@gmail.com>
2025-02-03 15:16:35 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+      <h1>It works!</h1>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
